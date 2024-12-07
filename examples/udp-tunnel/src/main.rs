134e290419f19798d6e59d568e7b8333a8020cc1
趙子賢 <danny900714@gmail.com>
2024-12-08 01:48:22 +0800

    Allow passing bind and destination socket address with command line arguments

--- examples/udp-tunnel/src/main.rs
+++ examples/udp-tunnel/src/main.rs
@@ -1,19 +1,17 @@
-use std::net::{SocketAddrV4, UdpSocket};
+use std::net::{SocketAddr, UdpSocket};
 use std::{env, io};
-
-const IP: &str = "0.0.0.0";
 
 fn main() {
     let args: Vec<_> = env::args().collect();
-    let port = args[1]
-        .parse::<u16>()
-        .expect("port must exist as the first arg");
+    let bind_ip = args[1]
+        .parse::<SocketAddr>()
+        .expect("bind socket address must exist as the first arg");
     let dest_ip = args[2]
-        .parse::<SocketAddrV4>()
+        .parse::<SocketAddr>()
         .expect("destination socket address must exist as the second arg");
 
-    println!("Binding on {}:{}/udp", IP, port);
-    let socket = UdpSocket::bind((IP, port)).expect("couldn't bind to address");
+    println!("Binding on {}/udp", bind_ip);
+    let socket = UdpSocket::bind(bind_ip).expect("couldn't bind to address");
 
     let mut stdin_buf = String::new();
     let stdin = io::stdin();
@@ -27,7 +25,9 @@ fn main() {
         stdin_buf.clear();
 
         let mut udp_buf = [0; 1024];
-        socket.recv_from(&mut udp_buf).expect("receiving should succeed");
+        socket
+            .recv_from(&mut udp_buf)
+            .expect("receiving should succeed");
         println!("{}", String::from_utf8_lossy(&udp_buf));
     }
 }
